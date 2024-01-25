e623bc4b5b64ca423768e270d185f6cdbcaa5b85
MdMahamudulHasan <hmahamudul846@gmail.com>
2024-01-26 01:30:43 +0600

    practice_DSA

---
+++ revise for dsa.py
@@ -0,0 +1,105 @@
+# print("Hello world")
+
+# a="Hello world"
+# print("Hello world")
+
+# print(a[1])
+# print(a[4])
+
+# for x in "banana":
+#     print(x)
+
+# a = "Hello world"
+# print(len(a))
+
+# txt = "The best things in life are free"
+# print("free" in txt)
+
+# txt = "The best things in life are free"
+# print("Expensive"  not in txt)
+
+# a =" Hello world"
+# print(a.upper())
+# print(a.lower())
+# print(a.strip())
+
+# print(a.replace("H", "J"))
+# print(a.split())
+# print(a.split(","))
+
+
+# quantity = 3
+# itemo = 567
+# price = 49.95
+# myorder = "I want {} peice of item {} for {} dollars"
+# print(myorder.format(quantity, itemo, price))
+
+# myorder2 = "I want to pay {2} dollars for {0} pieces of item {1}"
+# print(myorder2.format(quantity, itemo, price))
+
+# print(bool("hello"))
+# print(bool(15))
+
+
+# x = "World"
+# y = 30
+# print(bool(x))
+# print(bool(y))
+
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist)
+# print(thislist[1])
+# print(len(thislist))
+
+
+
+# thislist = ["apple", "banana", "cherry"]
+# if "apple" in thislist:
+#     print("Yes, apple is an th fruits list")
+
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1] = "blackcurrant"
+# print(thislist)
+
+# thislist1 = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# print(thislist1)
+# thislist1[1:3] = ["blackcurrant", "watermelon"]
+# print(thislist1)
+
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.append("orange")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.insert(1, "orange")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist1 = ["mango", "pineaple", "papaya"]
+# thislist.extend(thislist1)
+# print(thislist)
+
+
+# thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# thislist.sort()
+# print(thislist)
+# thislist.sort(reverse=True)
+# print(thislist)
+
+# thislist1 = [100, 50, 65, 82, 23]
+# thislist1.sort()
+# print(thislist1)
+# thislist1.sort(reverse=True)
+# print(thislist1)
+
+# def myfunc(n):
+#     return abs( n - 50)
+
+# thislist1.sort(key = myfunc)
+# print(thislist1)
+
+
+
